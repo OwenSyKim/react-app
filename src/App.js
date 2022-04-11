72bfbd98443b3ffa9b385cade308890918b4fc1e
OwenSyKim <owen.sykim@gmail.com>
2022-04-11 14:09:25 -0700

    add main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import Main from './components/Main.js'
+import logo from './img/teamhybridlogo.png';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <a target="_blank" href="http://localhost:3000/" role="mainlogo">
+        <img src={logo} />
+      </a>
+      <section>
+        <Main />
+      </section>
+    </>
   );
 }
 
-export default App;
+        export default App;
+
---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React, { useState, Fragment } from 'react';
+
+const Main = () => {
+
+
+return (
+    <>
+    Hello Ranmo
+    </>
+)
+
+}
+    
+
+
+
+
+export default Main;
